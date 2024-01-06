3970886129958cc80791680214de3fd72a86a521
Diego Gaspar <cadastro@diegogaspar.dev.br>
2024-01-06 16:24:01 -0300

    feat: add prometheus integration

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -5,13 +5,17 @@ import (
 
 	"github.com/gaspartv/financial-planning-system/adapter/http/actuator"
 	"github.com/gaspartv/financial-planning-system/adapter/http/transaction"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // Init is the initialization
 func Init() {
 	http.HandleFunc("/transactions", transaction.GetTransactions)
 	http.HandleFunc("/transactions/create", transaction.CreateTransaction)
+
 	http.HandleFunc("/health", actuator.Health)
+
+	http.Handle("/metrics", promhttp.Handler())
 
 	http.ListenAndServe(":8080", nil)
 }
